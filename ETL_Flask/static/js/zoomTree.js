fbad1d8406e75e5863d6faf4ef6312be8e416c4f
Araz Ohanessian <arazohanessian@Arazs-MacBook-Air.local>
2020-05-18 17:09:31 -0700

    araz changes

--- ETL_Flask/static/js/zoomTree.js
+++ ETL_Flask/static/js/zoomTree.js
@@ -1,80 +1,153 @@
-console.log(dataZT)
+var baseURL = "https://www.airbnb.com/rooms/"
 
+// function for determining change in input field
+function filterByInput(event) {
+  var newText = d3.select("#input-price").node().value;
+  console.log(newText)
+  filteredData = dataZT.filter(d => d.price <= newText)
+  d3.select("#input-price").node().value = ""
+  d3.selectAll("#chart > *").remove();
+  circlePack(hierarchy(filteredData));
+};
 
-//     d3.csv("./assets/data/data.csv").then(function(newsData) {
-//     console.log(newsData);
+// filtered data click function
+d3.select("#filter-btn").on("click", filterByInput);
 
-//     //data parsing
-//     newsData.age = +newsData.age
-//     newsData.income = +newsData.income
-//     newsData.obesity = +newsData.obesity
+//Function for ordering data in hierarchy
+function hierarchy(data) {
+  var newData = { name :"New York City", children : [] },
+    levels = ["neighborhood_group","neighborhood","room_type"];
 
-//     // scale y to chart height
-//     var yScale = d3.scaleLinear()
-//     .domain(d3.extent(newsData, d => d.obesity))
-//     .range([chartHeight, 0]);
+  data.forEach(function(d){
+    var depthCursor = newData.children;
+    levels.forEach(function( property, depth )
+    {
+        var index;
+        depthCursor.forEach(function(child,i)
+        {
+            if ( d[property] == child.name ) 
+                index = i;
+        });
 
-//     // scale x to chart width
-//     var xScale = d3.scaleLinear()
-//     .domain(d3.extent(newsData, d => d.age))
-//     .range([0, chartWidth]);
+        if ( isNaN(index) ) 
+        {
+            depthCursor.push({name : d[property], children : []});
+            index = depthCursor.length - 1;
+        }
 
-//     // create axes
-//     var yAxis = d3.axisLeft(yScale);
-//     var xAxis = d3.axisBottom(xScale);
+        depthCursor = depthCursor[index].children;
 
-//     // set x to the bottom of the chart
-//     chartGroup.append("g")
-//     .attr("transform", `translate(0, ${chartHeight})`)
-//     .call(xAxis);
+        if ( depth === levels.length - 1 )
+        {
+            depthCursor.push({name : `$${d.price}`, size : d.price, url : `${baseURL + d.entry_id}`, description : d.name});
+        }
+    });
+  })
+  return newData
+}
 
-//     // set y to the y axis
-//     chartGroup.append("g")
-//     .call(yAxis);
+//function for circle packing
+function circlePack(newData) {
 
-//     var ageLabel = chartGroup.append("text")
-//     // Position the text
-//     .attr("transform", `translate(${chartWidth / 2}, ${chartHeight+margin.bottom*(1/2) + 8})`)
-//     .text("Age (Median)")
-//     .attr("text-anchor", "middle")
-//     .attr("font-size", "16px")
-//     .attr("fill", "Black");
+  //define variables
+  var height = 700
+  var width = height
 
-//     var obesityLabel = chartGroup.append("text")
-//     .attr("transform", `translate(${(-margin.left)*(1/2)},${chartHeight/2})rotate(-90)`)
-//     .text("Obesity (%)")
-//     .attr("text-anchor", "middle")
-//     .attr("font-size", "16px")
-//     .attr("fill", "Black");
+  //Append svg to chart id in html
+  var svg = d3.select("#chart")
+      .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  var margin = 5
+  var diameter = +svg.attr("width")
 
+  // Append group element to svg
+  var g = svg
+    .append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
-//     // Create the rectangles using data binding
-//     var circlesGroup = chartGroup.selectAll("circle")
-//     .data(newsData)
-//     .enter()
-//     .append("circle")
-//     .attr("cx", (d, i) => xScale(newsData[i].age))
-//     .attr("cy", (d, i) => yScale(newsData[i].obesity))
-//     .attr("r", "10")
-//     .attr("stroke", "black")
-//     .attr("fill", "blue")
-//     .attr("opacity", 0.50);
+  var color = d3.scaleLinear()
+      .domain([-1, 5])
+      .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+      .interpolate(d3.interpolateHcl);
 
-//     var textGroup = chartGroup.selectAll(null)
-//     .data(newsData)
-//     .enter()
-//     .append("text")
-//     .attr("x", (d, i) => xScale(newsData[i].age))
-//     .attr("y", (d, i) => yScale(newsData[i].obesity))
-//     .text((d,i) => (newsData[i].abbr))
-//     .attr("font-size", "10px")
-//     .attr("text-anchor", "middle")
-//     .attr("alignment-baseline", "middle")
-//     .attr("fill", "white")
+  var pack = d3.pack()
+      .size([diameter - margin, diameter - margin])
+      .padding(2);
 
-//     })   
-// }
+  // create a tooltip
+  var toolTip = d3.select(".row").append("div")
+    .attr("class", "tooltip");
 
-// makeResponsive();
+  var root = d3.hierarchy(newData)
+      .sum(function(d) { return d.size; })
+      .sort(function(a, b) { return b.value - a.value; });
 
-// d3.select(window).on("resize", makeResponsive);
+  var focus = root,
+      nodes = pack(root).descendants(),
+      view;
+
+  // Append circles for data points
+  var circle = g.selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+      .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+      .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+      .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); })
+      .on("mouseover", function(d) {
+        toolTip.style("display", "block");
+        return d.children ? 
+        toolTip.html(`Category: <strong>${d.data.name}</strong>`) //if parent element
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px") :
+        toolTip.html(`Listing Description:<br> <strong>${d.data.description}</strong><br> *CLICK ME!*`) // if child element
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px")
+      })
+      .on("mouseout", function() {
+        toolTip.style("display", "none");
+      });
+  // text variable for circle text
+  var text = g.selectAll("text")
+    .data(nodes)
+    .enter().append("text")
+      .attr("class", function(d) { return d.parent ? d.children ? "label" : "label label--leaf" : "label label--root"; })
+      .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+      .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
+      .html(function(d) { return d.data.name; });
+
+  var node = g.selectAll("circle,text");
+
+  // On click functions for zoom and hyperlink
+  svg
+    .style("background", color(-1))
+    .on("click", function() {event.target.class === "node--leaf" ? zoom(root) : window.open(event.target.__data__.data.url); });
+
+  zoomTo([root.x, root.y, root.r * 2 + margin]);
+
+  // zoom function defined
+  function zoom(d) {
+    var focus0 = focus; focus = d;
+
+    var transition = d3.transition()
+      .duration(d3.event.altKey ? 7500 : 750)
+      .tween("zoom", function(d) {
+        var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+        return function(t) { zoomTo(i(t)); };
+      });
+
+    transition.selectAll("text")
+      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+      .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+      .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+      .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+  }
+
+  function zoomTo(v) {
+    var k = diameter / v[2]; view = v;
+    node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+    circle.attr("r", function(d) { return d.r * k; });
+  };
+
+}
+
+circlePack(hierarchy(dataZT));
